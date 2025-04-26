4f52ffd3aecaaac5897e00fa275f943819038741
ke <kehindeezekiel@gmail.com>
2025-04-26 15:52:06 -0700

    Add files via upload
    Hello World React Project..
    Objective: The goal of this assessment is to set up a basic React project and render a simple "Hello World" message in the web browser. This will test ability to initialize a React project and understand fundamental React concepts.

---
+++ App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <main style={{
+      display: 'flex',
+      minHeight: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f0f4f8',
+      fontFamily: 'Arial, sans-serif',
+    }}>
+      <h1 style={{
+        fontSize: '3rem',
+        color: '#333',
+      }}>
+        Hello World
+      </h1>
+    </main>
+  );
+}
+
+export default App;
